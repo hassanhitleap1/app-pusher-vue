c388bd1674f32c771896e5293d0bc22a277d405b
hassan kiwan <hassanhitleap1@gmail.com>
2021-04-07 14:23:51 +0300

    5

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Model\Category;
+use App\Model\Question;
+use App\Model\Reply;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(User::class,20)->create();
+        factory(Category::class,5)->create();
+        factory(Question::class,10)->create();
+        factory(Reply::class,50)->create()->each(function ($reply){
+            return $reply->likes->save(factory(User::class,20)->make());
+        });
     }
 }
