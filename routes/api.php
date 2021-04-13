d19b61f516fb92425fc12277baf73271eb5f8f44
hassan kiwan <hassanhitleap1@gmail.com>
2021-04-13 11:29:14 +0300

    10

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,16 @@ Route::apiResource('questions', App\Http\Controllers\QuestionController::class);
 Route::apiResource('categories', App\Http\Controllers\CategoryController::class);
 Route::apiResource('questions/{question}/reply', App\Http\Controllers\ReplyController::class);
 
-Route::post('like/{reply}', App\Http\Controllers\LikesController::class ,'like');
-Route::delete('like/{reply}', App\Http\Controllers\LikesController::class ,'unLikeit');
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+
+//Route::post('like/{reply}', App\Http\Controllers\LikesController::class ,'like');
+//Route::delete('like/{reply}', App\Http\Controllers\LikesController::class ,'unLikeit');
