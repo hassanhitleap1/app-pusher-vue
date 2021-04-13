b1023c4b47a3d98aaf5df88fb2187edccf4a4160
hassan kiwan <hassanhitleap1@gmail.com>
2021-04-13 11:00:10 +0300

    10

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('questions', App\Http\Controllers\QuestionController::class);
 Route::apiResource('categories', App\Http\Controllers\CategoryController::class);
 Route::apiResource('questions/{question}/reply', App\Http\Controllers\ReplyController::class);
+
+Route::post('like/{reply}', App\Http\Controllers\LikesController::class ,'like');
+Route::delete('like/{reply}', App\Http\Controllers\LikesController::class ,'unLikeit');
